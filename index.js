1cb0e6021a648f7911f742e1eaaa4148166b39fb
Assitan Koné <assitan.kone@gmail.com>
2019-01-15 10:50:29 +0100

    Add type

--- index.js
+++ index.js
@@ -2,17 +2,56 @@ const {
   GraphQLServer
 } = require('graphql-yoga');
 
+// Scalar type : type that stores a single value - String, Boolean, Int, Float, ID
+
 // Type definitions (schema) - name of the query and the type that should back
 const typeDefs = `
   type Query {
-    hello(name: String): String!
+    me: User!
+    post: Post!
+    hello(name: String, email: String): String!
+    height: Float
+    isNice: Boolean
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    age(num: Int): Int!
+  }
+
+  type Post {
+    id: ID!
+    title: String!
+    body: String!
+    published: Boolean
   }
 `
 
 // Resolvers (set of functions)
 const resolvers = {
   Query: {
-    hello: (_, { name }) => `Hello ${name || 'World'}`,
+    me() {
+      return {
+        id: '122MK22',
+        name: 'Assitan',
+        email: 'rerer@gmail.com',
+        age: '29'
+      };
+    },
+    post() {
+      return {
+        id: '2E3D',
+        title: 'Les oiseaux',
+        body: 'Lorem Ipsum',
+        // published: false, renvoie null car pas obligatoire
+      };
+    },
+    // root : parents, usefull for relational data. Name : arguments contains the info we need, ctx, info
+    hello: (root, { name, email }) => `Hello ${name || 'World'}. Your email : ${email}`,
+    height: () => 1.74,
+    isNice: () => true,
   },
 };
 
